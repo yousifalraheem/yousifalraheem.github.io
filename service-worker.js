8c9d1220a25f6b10185f2fc2529539de918e8da1
Yousif Al-Raheem <yousifalraheem@gmail.com>
2019-02-22 00:21:45 +0800

    New UI, full resume

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4b4c384d83ecd049f0c0d535fa2cdd47.js"
+  "./precache-manifest.01a53bf1d2d77c9da42aaf25f99aa390.js"
 );
 
 workbox.clientsClaim();

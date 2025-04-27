c72fd1b94bbf67cf5d976d2211a1863e10a1ad42
Yousif Al-Raheem <yousifalraheem@gmail.com>
2025-04-27 22:37:35 +0200

    completed space section
    Signed-off-by: Yousif Al-Raheem <yousifalraheem@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
 import type { PropsWithChildren } from "react";
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata, Viewport } from "next";
+import "../styles/globals.css";
 
 export const metadata: Metadata = {
   title: "Portfolio - Yousif Al-Raheem",
   description: "Portfolio of Yousif Al-Raheem",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({ children }: PropsWithChildren) {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Space from "@/components/Space/Space";
+
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <Space />
+    </>
+  );
 }
